b84b77f6a6d58cc4dad2df7652f0daa1f9f2d360
Sam <lemonverse@gmail.com>
2020-06-12 12:19:47 +1200

    Begin!

---
+++ src/main/java/party/lemons/adventuringparty/AdventuringParty.java
@@ -0,0 +1,12 @@
+package party.lemons.adventuringparty;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AdventuringParty implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
